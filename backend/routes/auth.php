d89d5885fc88b956a0889c6474ea1fff7bf75ab5
fabuandras <fabuandras@gmail.com>
2026-03-07 21:31:05 +0100

    merge javitva

--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -6,9 +6,10 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [RegisteredUserController::class, 'store'])
+Route::post('/register', [UserController::class, 'register'])
     ->middleware('guest')
     ->name('register');
 
